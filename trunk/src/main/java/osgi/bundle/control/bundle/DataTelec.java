06a018b4022a0ac11a300123d2c66c8fe2716b00
joachim.segala@gmail.com <joachim.segala@gmail.com@cdcd1adf-4079-7574-7331-7115941aec0e>
2012-06-26 14:31:19 +0000

    réalisation partielle du long polling sur l'ensemble de l'interface

--- trunk/src/main/java/osgi/bundle/control/bundle/DataTelec.java
+++ trunk/src/main/java/osgi/bundle/control/bundle/DataTelec.java
@@ -11,47 +11,50 @@ import java.util.ArrayList;
  * @author Joachim
  */
 public class DataTelec {
+
     private ArrayList<User> users;
     private ArrayList<Device> devices;
     private ArrayList<Device> map;
-    
-    public DataTelec(){
+
+    public DataTelec() {
         users = new ArrayList<User>();
         devices = new ArrayList<Device>();
         map = new ArrayList<Device>();
     }
-    
-    public void addUser(User u){
-        if(!users.contains(u))
+
+    public void addUser(User u) {
+        if (!users.contains(u)) {
             users.add(u);
+        }
     }
-    
-    public void addDevice(Device d){
-        if(!devices.contains(d))
+
+    public void addDevice(Device d) {
+        if (!devices.contains(d)) {
             devices.add(d);
+        }
     }
-    
-    public void addMap(Device d){
-        if(!map.contains(d)){
+
+    public void addMap(Device d) {
+        if (!map.contains(d)) {
             map.add(d);
-        }else{
+        } else {
             map.get(map.indexOf(d)).setX(d.getX());
             map.get(map.indexOf(d)).setY(d.getY());
         }
     }
-    
-    public void removeUser(User u){
+
+    public void removeUser(User u) {
         users.remove(u);
     }
-    
-    public void removeDevice(Device d){
+
+    public void removeDevice(Device d) {
         devices.remove(d);
     }
-    
-    public void removeMap(Device d){
+
+    public void removeMap(Device d) {
         map.remove(d);
     }
-    
+
     public ArrayList<Device> getDevices() {
         return devices;
     }
@@ -61,70 +64,76 @@ public class DataTelec {
     }
 
     public String initUsers() {
-        String res="function init_users(){"+   
-                    "var l = new List(document.getElementById('userschildren'),20,'vertical');";
-        for(User u : users){
+        String res = "function init_users(){"
+                + "var l = new List(document.getElementById('userschildren'),20,'vertical');"
+                + "var user_list = new Array();";
+        for (User u : users) {
             res += u.toString();
         }
-        res+= "return l;}";
+        res += "return user_list;}";
         return res;
     }
-    
-    public String initDevices(){
-        String res = "function init_devices(){"+
-              "var l = new List(document.getElementById('debarraschildren'),20,'vertical');";
-        for(Device d : devices){
+
+    public String initDevices() {
+        String res = "function init_devices(){"
+                + "var l = new List(document.getElementById('debarraschildren'),20,'vertical');";
+        for (Device d : devices) {
             res += d.toString();
         }
         res += "return l;}";
         return res;
     }
-    
-    public String initMap(){
+
+    public String initMap() {
         String res = "";
-        if(map.size() !=0){
-            for(Device d : map){
-                res += "var d1 = new Device('"+d.getId()+"','html/"+d.getImg()+"');"+
-                       "mapContainer.add(d1);"+
-                       "d1.setAttribute('transform','translate("+d.getX()+","+d.getY()+")');"+
-                       "Draggable(d1.id,[d1.id], startDragElement, null,  null);";
+        if (map.size() != 0) {
+            for (Device d : map) {
+                res += "var d1 = new Device('" + d.getId() + "','html/" + d.getImg() + "');"
+                        + "mapContainer.add(d1);"
+                        + "d1.setAttribute('transform','translate(" + d.getX() + "," + d.getY() + ")');"
+                        + "Draggable(d1.id,[d1.id], startDragElement, null,  null);";
             }
         }
         return res;
     }
-    
-    public String init(){
-        return "function init_every() {"+
-               "init_draggable();"+
-               "var userContainer = new Container('users',true,window.innerWidth/2-window.innerWidth/120,window.innerHeight/2-window.innerHeight/60,'translate(0,0)');"+
-               "var user_list = init_users();"+
-               "debarrasContainer = new Container('debarras',true,window.innerWidth/2-window.innerWidth/120,window.innerHeight/2-window.innerHeight/60,'translate(0,'+62*window.innerHeight/120+')');"+
-               "var devices_list = init_devices();"+
-               "debarrasContainer.setDropZone(devices_list);"+
-               "var mapContainer = new Container('plan',false,window.innerWidth/2-window.innerWidth/120,4*window.innerHeight/5-window.innerHeight/60,'translate('+122*window.innerWidth/240+','+65*window.innerHeight/300+')');"+
-               "mapContainer.setDropZone();"+
-               initMap()+
-               "var cloudContainer = new Container('cloud',false,window.innerWidth/2-window.innerWidth/120,window.innerHeight/5-window.innerHeight/60,'translate('+122*window.innerWidth/240+',0)');"+
-               "var sepNSO = new Separator('separatornordsudouest', document.getElementById('main'),  'horizontal', [document.getElementById(userContainer.id)],  [document.getElementById(debarrasContainer.id)], document.getElementById(userContainer.id));"+
-               "var sepEO = new Separator('separatorestouest', document.getElementById('main'),  'vertical', [document.getElementById(userContainer.id),document.getElementById(debarrasContainer.id)],  [document.getElementById(cloudContainer.id),document.getElementById(mapContainer.id)], document.getElementById('main'));"+
-               "var sepNSE = new Separator('separatornordsudest', document.getElementById('main'),  'horizontal', [document.getElementById(cloudContainer.id)],  [document.getElementById(mapContainer.id)], document.getElementById(cloudContainer.id));"+
-               "}";
+
+    public String init() {
+        return "function init_every() {"
+                + "init_draggable();"
+                + "var userContainer = new Container('users',true,window.innerWidth/2-window.innerWidth/120,window.innerHeight/2-window.innerHeight/60,'translate(0,0)');"
+                + "var user_list = init_users();"
+                + "debarrasContainer = new Container('debarras',true,window.innerWidth/2-window.innerWidth/120,window.innerHeight/2-window.innerHeight/60,'translate(0,'+62*window.innerHeight/120+')');"
+                + "var devices_list = init_devices();"
+                + "debarrasContainer.setDropZone(devices_list);"
+                + "var mapContainer = new Container('plan',false,window.innerWidth/2-window.innerWidth/120,4*window.innerHeight/5-window.innerHeight/60,'translate('+122*window.innerWidth/240+','+65*window.innerHeight/300+')');"
+                + "mapContainer.setDropZone();"
+                + initMap()
+                + "var cloudContainer = new Container('cloud',false,window.innerWidth/2-window.innerWidth/120,window.innerHeight/5-window.innerHeight/60,'translate('+122*window.innerWidth/240+',0)');"
+                + "var sepNSO = new Separator('separatornordsudouest', document.getElementById('main'),  'horizontal', [document.getElementById(userContainer.id)],  [document.getElementById(debarrasContainer.id)], document.getElementById(userContainer.id));"
+                + "var sepEO = new Separator('separatorestouest', document.getElementById('main'),  'vertical', [document.getElementById(userContainer.id),document.getElementById(debarrasContainer.id)],  [document.getElementById(cloudContainer.id),document.getElementById(mapContainer.id)], document.getElementById('main'));"
+                + "var sepNSE = new Separator('separatornordsudest', document.getElementById('main'),  'horizontal', [document.getElementById(cloudContainer.id)],  [document.getElementById(mapContainer.id)], document.getElementById(cloudContainer.id));"
+                + "var tmp = document.createElementNS('http://www.w3.org/2000/svg','text');"
+                + "tmp.setAttribute('id','text_tmp');"
+                + "tmp.setAttribute('x','20');"
+                + "tmp.setAttribute('y','20');"
+                + "tmp.appendChild(document.createTextNode('coucou'));"
+                + "document.getElementById('plan').appendChild(tmp);"
+                + "getAjax(user_list, devices_list);"
+                + "}";
     }
 
     @Override
     public String toString() {
-        return initUsers()+initDevices()+init();
+        return initUsers() + initDevices() + init();
     }
 
     void add(Device device, User user) {
-        users.get(users.indexOf(user)).addDevice(device);        
+        users.get(users.indexOf(user)).addDevice(device);
     }
 
     void removeUserDevice(Device device) {
-        for(User u : users){
+        for (User u : users) {
             u.removeDevice(device);
         }
     }
-    
-    
 }
--- trunk/src/main/java/osgi/bundle/control/bundle/User.java
+++ trunk/src/main/java/osgi/bundle/control/bundle/User.java
@@ -11,34 +11,36 @@ import java.util.ArrayList;
  * @author Joachim
  */
 class User {
+
     private String id;
     private String name;
     private String img;
     private ArrayList<Device> devices;
-    
-    public User(String id, String img){
+
+    public User(String id, String img) {
         this.id = id;
         this.name = id;
         this.img = img;
         devices = new ArrayList<Device>();
     }
-    
-    public User(String id, String img, String name){
+
+    public User(String id, String img, String name) {
         this.id = id;
         this.img = img;
         this.name = name;
         devices = new ArrayList<Device>();
     }
 
-    public void addDevice(Device d){
-        if(!devices.contains(d))
+    public void addDevice(Device d) {
+        if (!devices.contains(d)) {
             devices.add(d);
+        }
     }
-    
-    public void removeDevice(Device d){
+
+    public void removeDevice(Device d) {
         devices.remove(d);
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -57,16 +59,14 @@ class User {
     @Override
     public String toString() {
         String tab = "var devices = new Array();";
-        for(Device d : devices){
-            tab += "var d = new Device('"+d.getId()+"','html/"+d.getImg()+"');"
+        for (Device d : devices) {
+            tab += "var d = new Device('" + d.getId() + "','html/" + d.getImg() + "');"
                     + "devices.push(d);";
         }
-        return tab +"var u1 = new User('"+id+"','html/"+img+"',devices, '"+name+"');"+
-        "document.getElementById('userschildren').appendChild(u1.node);"+
-        "u1.setDropZone(new List(document.getElementById(u1.node.id),0,'horizontal'));"+
-        "l.add(u1.node);";
+        return tab + "var u1 = new User('" + id + "','html/" + img + "',devices, '" + name + "');"
+                + "document.getElementById('userschildren').appendChild(u1.node);"
+                + "u1.setDropZone(new List(document.getElementById(u1.node.id),0,'horizontal'));"
+                + "l.add(u1.node);"
+                + "user_list.push(u1);";
     }
-    
-    
-    
 }
